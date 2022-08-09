8cc1576827800b5b331eb288f1c096e784690f13
jake0sand <79411811+jake0sand@users.noreply.github.com>
2022-08-09 07:55:32 -0600

    final

--- app/src/test/java/com/jakey/aetnacompose/data/remote/responses/ItemTest.kt
+++ app/src/test/java/com/jakey/aetnacompose/data/remote/responses/ItemTest.kt
@@ -10,7 +10,7 @@ class ItemTest {
     @Test
     fun `item mapper correctly maps to Detail Item`() {
         val testItem = Item(
-            author = "Jake",
+            author = "nobody@flickr.com (\"Jake\")",
             media = Media(m = "https://www.youneverwalkalone.com/image.jpg"),
             title = "Sweet title, dude",
             description = "Thank you so much for this opportunity ):" +
